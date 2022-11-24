ccb5aa39dada3606c01205a23832f5ff8010e385
PHAMDUCOANH\FPTSHOP <oanhpdph25707@fpt.edu.vn>
2022-11-24 23:00:53 +0700

    no message

---
+++ QLPhongKhachSan/src/model/PromotionR.java
@@ -0,0 +1,52 @@
+package model;
+
+public class PromotionR {
+
+    private String id;
+    private String value;
+    private String dateStart;
+    private String dateEnd;
+
+    public PromotionR() {
+    }
+
+    public PromotionR(String id, String value, String dateStart, String dateEnd) {
+        this.id = id;
+        this.value = value;
+        this.dateStart = dateStart;
+        this.dateEnd = dateEnd;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getDateStart() {
+        return dateStart;
+    }
+
+    public void setDateStart(String dateStart) {
+        this.dateStart = dateStart;
+    }
+
+    public String getDateEnd() {
+        return dateEnd;
+    }
+
+    public void setDateEnd(String dateEnd) {
+        this.dateEnd = dateEnd;
+    }
+
+}
