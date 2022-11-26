832183cd3220410ecf94deda1d9daaa17557a159
PHAMDUCOANH\FPTSHOP <oanhpdph25707@fpt.edu.vn>
2022-11-26 20:42:21 +0700

    Oanh update lại db. Sửa lại các trường thành not null nên không ảnh hưởng đến hiện tại

--- QLPhongKhachSan.sql
+++ QLPhongKhachSan.sql
@@ -1,4 +1,4 @@
-if OBJECT_ID('QLPhongKhachSan') is not null
+﻿if OBJECT_ID('QLPhongKhachSan') is not null
 drop database QLPhongKhachSan
 go
 create database QLPhongKhachSan
@@ -11,12 +11,12 @@ drop table Client
 go
 create table Client
 (id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
-code varchar(20) unique,
-[name] nvarchar(50),
-dateOfBirth date,
-sex nvarchar(5),
-[address] nvarchar(200),
-idPersonCard varchar(12) unique,
+code varchar(20) unique not null,
+[name] nvarchar(50) not null,
+dateOfBirth date not null,
+sex nvarchar(5) not null,
+[address] nvarchar(200) not null,
+idPersonCard varchar(12) unique not null,
 customPhone varchar(10) 
 )
 
@@ -26,16 +26,16 @@ drop table Staff
 go
 create table Staff
 (id Uniqueidentifier primary key default newid(),
-code varchar(20) unique,
-[name] nvarchar(50),
-dateOfBirth date,
-sex nvarchar(5),
-[address] nvarchar(200),
-idPersonCard varchar(12) unique,
+code varchar(20) unique not null,
+[name] nvarchar(50) not null,
+dateOfBirth date not null,
+sex nvarchar(5) not null,
+[address] nvarchar(200) not null,
+idPersonCard varchar(12) unique not null,
 phone varchar(10),
-[user] varchar(30) unique,
-pass varchar(20),
-[rule] varchar(20)
+[user] varchar(30) unique  not null,
+pass varchar(20)  not null,
+[rule] varchar(20)  not null
 )
 
 
@@ -48,13 +48,13 @@ create table bill
 id Uniqueidentifier primary key default newid(),
 idClient Uniqueidentifier ,
 idStaff Uniqueidentifier,
-code varchar(20) unique,
-[name] nvarchar(50),
+code varchar(20) unique  not null,
+[name] nvarchar(50)  not null,
 Price DECIMAL(20,0) DEFAULT 0,
-[address] nvarchar(200),
-hotelPhone varchar(10),
-[status] int default 0,
-[date] smalldatetime,
+[address] nvarchar(200) not null,
+hotelPhone varchar(10) not null,
+[status] int default 0,-- 0 là chưa thanh toán, 1 là đã thanh toán
+[date] smalldatetime not null,
 constraint FK_idClient foreign key (idClient ) references Client(id),
 constraint FK_idStaff foreign key (idStaff ) references Staff(id),
 )
@@ -67,8 +67,8 @@ drop table Item
 go
 create table item
 (id uniqueidentifier primary key default newid(),
-code varchar(20),
-[name] nvarchar(50)
+code varchar(20)  not null,
+[name] nvarchar(50)  not null
 )
 
 -- table promotionsService
@@ -79,8 +79,8 @@ create table promotionS
 (Id uniqueidentifier primary key default newid(),
 code varchar(10) unique,
 [value] decimal(20,0) default 0,
-dateStart date,
-dateEnd date,
+dateStart date  not null,
+dateEnd date  not null,
 )
 
 
@@ -91,11 +91,11 @@ go
 create table [service]
 (
 id uniqueidentifier primary key default newid(),
-idpromotion uniqueidentifier,
-code varchar(20) unique,
-[name] nvarchar(50),
+idpromotion uniqueidentifier  null,
+code varchar(20) unique  not null,
+[name] nvarchar(50)  not null,
 price decimal(20,0) default 0,
-[notes] nvarchar(50),
+[notes] nvarchar(50)  not null,
 constraint fk_idpromotionS foreign key (idpromotion) references promotionS(id)
 )
 
@@ -106,10 +106,10 @@ drop table promotionR
 go
 create table promotionR
 (Id uniqueidentifier primary key default newid(),
-code varchar(10) unique,
+code varchar(10) unique  not null,
 [value] decimal(20,0) default 0,
-dateStart date,
-dateEnd date,
+dateStart date  not null,
+dateEnd date  not null,
 )
 
 
@@ -122,10 +122,10 @@ create table Room
 [status] int default 1,-- 1 san sang, 2 co khach, 3 chua don, 4 dang don, 5 dang sua
 KindofRoom int default 1,-- 1 phong don, 2 phong doi, 3 phong vip
 idPromotion uniqueidentifier,
-code varchar(20) unique,
-roomNumber varchar(10),
-area varchar(10),
-[location] nvarchar(50),
+code varchar(20) unique  not null,
+roomNumber varchar(10)  not null,
+area varchar(10)  not null,
+[location] nvarchar(50)  not null,
 price decimal(20,0) default 0
 constraint fk_idPromotion foreign key (idPromotion) references promotionR(id)
 )
@@ -135,9 +135,9 @@ if OBJECT_ID('roomItem') is not null
 drop table roomItem
 go
 create table roomItem
-(roomId uniqueidentifier,
-itemId uniqueidentifier,
-[status] nvarchar(50),
+(roomId uniqueidentifier  not null,
+itemId uniqueidentifier  not null,
+[status] nvarchar(50)  not null,
 amount int default 0
 constraint pk_roomItem primary key (roomId,itemId),
 constraint fk_roomid foreign key (roomid) references room(id),
@@ -150,12 +150,12 @@ if OBJECT_ID('RoomBill') is not null
 drop table RoomBill
 go
 create table RoomBill
-(RoomId uniqueidentifier,
-BillId uniqueidentifier unique,
+(RoomId uniqueidentifier  not null,
+BillId uniqueidentifier unique  not null,
 priceRoom decimal(20,0) default 0,
 promotionRoom decimal(20,0) default 0,
-dateCheckIn smalldatetime,
-dateCheckOut smalldatetime
+dateCheckIn smalldatetime  not null,
+dateCheckOut smalldatetime not null
 conStraint pk_roombill primary key(RoomId,BillId),
 constraint fk_roomId1 foreign key (roomid) references room(id),
 constraint fk_BillId foreign key (billid) references bill(id),
@@ -167,13 +167,13 @@ if OBJECT_ID('RoomBillService') is not null
 drop table RoomBillService
 go
 create table RoomBillService
-(IdBill uniqueidentifier,
-IdService uniqueidentifier,
-IdRoom uniqueidentifier,
+(IdBill uniqueidentifier  not null,
+IdService uniqueidentifier not null,
+IdRoom uniqueidentifier not null,
 priceService decimal(20,0) default 0,
 promotionService decimal(20,0) default 0,
-dateOfHire nvarchar(200),
-times int
+dateOfHire nvarchar(200) not null,
+times int not null
 constraint pk_roomBillService primary key(IdBill,IdService,IdRoom),
 constraint fk_idBill foreign key (idbill) references roomBill(BillId),
 constraint fk_idService foreign key (idservice) references [service](id),
